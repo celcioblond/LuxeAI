5b0aba46c41d783a2deec99561e1f31c8e166659
mateo <mateoblondetz@gmail.com>
2026-03-28 17:15:23 -0500

    Modified frontend setup including pages and routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import Homepage from "./pages/Core/Homepage";
 import Register from "./pages/Auth/Register";
 
 const App = () => {
@@ -6,9 +7,11 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/" element={<Navigate to="/homepage" />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
