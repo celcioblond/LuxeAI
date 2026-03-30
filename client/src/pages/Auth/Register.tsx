3b6a7327b9154ae7f083b5dfb899f5846f6739d5
mateo <mateoblondetz@gmail.com>
2026-03-29 21:16:36 -0500

    Prepared the register form setup

--- client/src/pages/Auth/Register.tsx
+++ client/src/pages/Auth/Register.tsx
@@ -1,21 +1,46 @@
-import { useRef, useState, useEffect} from "react";
-
-const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&]).{8,24}$/;
 
 const Register = () => {
 
-  const userRef = useRef();
-  const errorRef = useRef();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Submitted");
 
-  const [user, setUser] = useState('');
-  const [validName, setValidName] = useState(false);
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+    e.currentTarget.reset();
+  }
+
 
   return (
-    <div>
-      <h1>register page</h1>
-    </div>
-  )
+    <form onSubmit={handleSubmit}>
+      <div>
+        <div>
+          <h2>Register</h2>
+        </div>
+        <div>
+
+          <div>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" placeholder="Name" required />
+          </div>
+
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Email" required/>
+          </div>
+
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Password" required/>
+          </div>
+
+          <div>
+            <button type="submit">Register</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 }
 
 export default Register;
