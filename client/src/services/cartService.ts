537f02e2dd8360e104800967f8cacff437afb5ef
mateo <mateoblondetz@gmail.com>
2026-05-03 15:43:41 -0500

    Implemented cartServices to get the data from the server

--- client/src/services/cartService.ts
+++ client/src/services/cartService.ts
@@ -1,20 +1,74 @@
-import axios, {AxiosError} from "axios";
-const BACKEND_API = import.meta.env.VITE_API_URL;
+import axios from "axios";
+const BACKEND_URL = import.meta.env.VITE_API_URL;
 
-interface CartResponse{
+interface infoCart{
   userId: string;
-  products: string[];
+  productId: string;
+  quantity: number;
 }
 
-const cartService = async() => {
-  try { 
-    const response = await axios.post<CartResponse>(
-      `${BACKEND_API}`}
-    )
-  } catch(error) {
-    const err = error as AxiosError<{message?: string}>;
+interface user{
+  userId: string;
+}
 
+
+interface userProduct{
+  userId: string;
+  productId: string;
+}
+
+export const getCartService = async(userId: string) => {
+  try {
+    const response = await axios.get(`${BACKEND_URL}/getCart/${userId}`);
+    return response.data;
+  } catch(error) {
+    throw new Error(`${error.message}`);
   }
 }
 
-export default cartService;
+export const addToCartService = async(info: infoCart) => {
+  try {
+    const response = await axios.post(`${BACKEND_URL}/addToCart`, info);
+    return response.data;
+  } catch(error) {
+    throw new Error(`${error.message}`);
+  }
+}
+
+export const updateCartService = async(info: infoCart) => {
+  const {userId, ...body} = info;
+  try {
+    const response = await axios.put(`${BACKEND_URL}/updateCart/${userId}`, body);
+    return response.data;
+  } catch(error) {
+    throw new Error(`${error.message}`);
+  }
+}
+
+export const deleteProductService = async(info: userProduct) => {
+  const {userId, productId} = info;
+  try {
+    const response = await axios.delete(`${BACKEND_URL}/deleteProduct/${userId}/${productId}`);
+    return response.data; 
+  } catch(error) {
+    throw new Error(`${error.message}`);
+  }
+}
+
+export const getCartTotalService = async(userId: string) => {
+  try {
+    const response = await axios.get(`${BACKEND_URL}/total/${userId}`);
+    return response.data;
+  } catch(error) {
+    throw new Error(`${error.message}`);
+  }
+}
+
+export const clearCartService = async(userId: string) => {
+  try {
+    const response = await axios.delete(`${BACKEND_URL}/clearCart/${userId}`);
+    return response.data;
+  } catch(error){
+    throw new Error(`${error.message}`);
+  }
+}
