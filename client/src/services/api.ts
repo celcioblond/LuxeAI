ae2c03e5f273ed1ba635ee1133330677c062530e
mateo <mateoblondetz@gmail.com>
2026-05-24 18:15:07 -0500

    Created shared axios instance with auth token

---
+++ client/src/services/api.ts
@@ -0,0 +1,25 @@
+import axios, { type AxiosInstance } from 'axios';
+const BACK_URL = import.meta.env.VITE_API_URL;
+
+//get token from localstorage
+
+export const getToken = (): string | null => {
+  const token = localStorage.getItem('token');
+  return token ? JSON.parse(token) : null;
+};
+
+const api: AxiosInstance = axios.create({
+  baseURL: BACK_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use((config) => {
+  if (getToken()) {
+    config.headers.Authorization = `Bearer ${getToken()}`;
+  }
+  return config;
+});
+
+export default api;
