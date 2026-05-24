d84e9ca49e1c33bb9a60cd51061bfefdbca5cad0
mateo <mateoblondetz@gmail.com>
2026-05-24 15:49:12 -0500

    Refactored controller functions

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,6 +1,6 @@
-import User from "../models/userModel.js";
-import Product from "../models/productModel.js";
-import HttpError from "../models/http-error.js";
+import User from '../models/userModel.js';
+import Product from '../models/productModel.js';
+import HttpError from '../models/http-error.js';
 
 export const getDashboardStats = async (req, res, next) => {
   try {
@@ -16,7 +16,7 @@ export const getDashboardStats = async (req, res, next) => {
 
 export const getAllUsers = async (req, res, next) => {
   try {
-    const users = await User.find().select("-password");
+    const users = await User.find().select('-password');
     res.status(200).json({ users });
   } catch (error) {
     return next(new HttpError(`Failed to get users: ${error.message}`, 500));
@@ -28,33 +28,52 @@ export const updateUser = async (req, res, next) => {
     const { id } = req.params;
     const { name, email, role, address } = req.body;
 
-    const updates = {};
-    if (name !== undefined)  updates.name  = name;
-    if (email !== undefined) updates.email = email;
-    if (role !== undefined)  updates.role  = role;
+    const updatedData = {};
+    if (name !== undefined) updateData.name = name;
+    if (email !== undefined) updateData.email = email;
+    if (role !== undefined) updateData.role = role;
 
     if (address) {
-      const fields = ['street', 'city', 'state', 'zip', 'country'];
-      fields.forEach(field => {
-        if (address[field] !== undefined) updates[`address.${field}`] = address[field];
-      });
-    }
+      if (address.street !== undefined) {
+        updateData['address.street'] = address.street;
+      }
 
-    const user = await User.findByIdAndUpdate(
-      id,
-      { $set: updates },
-      { new: true, runValidators: true }
-    ).select("-password");
+      if (address.city !== undefined) {
+        updateData['address.city'] = address.city;
+      }
+
+      if (address.state !== undefined) {
+        updateData['address.state'] = address.state;
+      }
+
+      if (address.zip !== undefined) {
+        updateData['address.zip'] = address.zip;
+      }
+
+      if (address.country !== undefined) {
+        updateData['address.country'] = address.country;
+      }
+
+      const user = await User.findByIdAndUpdate(
+        id,
+        {$set: updatedData},
+        {
+          new: true,
+          runValidators: true,
+        }
+      ).select("-password");
+    }
 
     if (!user) {
-      return next(new HttpError("User not found", 404));
+      return next(new HttpError("User not foudn", 404));
     }
 
-    res.status(200).json({ user });
+    res.status(200).json({user});
   } catch (error) {
     return next(new HttpError(`Failed to update user: ${error.message}`, 500));
   }
 };
+
 
 export const deleteUser = async (req, res, next) => {
   try {
@@ -62,10 +81,10 @@ export const deleteUser = async (req, res, next) => {
     const user = await User.findByIdAndDelete(id);
 
     if (!user) {
-      return next(new HttpError("User not found", 404));
+      return next(new HttpError('User not found', 404));
     }
 
-    res.status(200).json({ message: "User deleted successfully" });
+    res.status(200).json({ message: 'User deleted successfully' });
   } catch (error) {
     return next(new HttpError(`Failed to delete user: ${error.message}`, 500));
   }
