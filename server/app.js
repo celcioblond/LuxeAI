c000c9cc037167a349394e0e09badc6538f78fdb
mateo <mateoblondetz@gmail.com>
2026-05-23 19:06:03 -0500

    chore: remove deleted route and controller references from server

--- server/app.js
+++ server/app.js
@@ -2,10 +2,7 @@ import express from "express"
 import morgan from "morgan";
 import auth from "./routes/auth.js";
 import products from "./routes/products.js";
-import orders from "./routes/orders.js";
 import cart from "./routes/cart.js";
-import reviews from "./routes/reviews.js";
-import admin from "./routes/admin.js";
 import cors from "cors";
 import bodyParser from "body-parser";
 import HttpError from "./models/http-error.js";
@@ -35,10 +32,7 @@ app.use(express.json());
 
 app.use("/api/auth",     auth);
 app.use("/api/products", products);
-app.use("/api/orders",   orders);
 app.use("/api/cart",     cart);
-app.use("/api/reviews",  reviews);
-app.use("/api/admin",    admin);
 
 app.use((_req, _res, next) => {
   next(new HttpError("Could not find this route", 404));
