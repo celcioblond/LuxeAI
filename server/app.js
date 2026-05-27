3204803b61ce9e385c4879959ca02a5718b0c5cf
mateo <mateoblondetz@gmail.com>
2026-05-27 15:58:47 -0500

    feat(server): scaffold orders route and controller, wire up Stripe webhook

--- server/app.js
+++ server/app.js
@@ -1,50 +1,52 @@
-import express from "express"
-import morgan from "morgan";
-import auth from "./routes/auth.js";
-import products from "./routes/products.js";
-import cart from "./routes/cart.js";
-import admin from "./routes/admin.js";
-import cors from "cors";
-import bodyParser from "body-parser";
-import HttpError from "./models/http-error.js";
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-import { connectDB } from "./config/db.js";
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { connectDB } from './config/db.js';
+import HttpError from './models/http-error.js';
+import admin from './routes/admin.js';
+import auth from './routes/auth.js';
+import cart from './routes/cart.js';
+import orders from './routes/orders.js;';
+import products from './routes/products.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: path.join(__dirname, ".env") });
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const PORT = process.env.PORT;
 const FRONT_URL = process.env.FRONT_URL;
 const app = express();
 
-app.use(cors({
-  origin: FRONT_URL,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-  ],
-  credentials: true,
-}));
-app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: FRONT_URL,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }),
+);
+app.use(morgan('dev'));
+
+app.use('/api/webhooks/stripe', express.raw({ type: 'application/json' }));
 app.use(express.json());
 
-app.use("/api/auth",     auth);
-app.use("/api/products", products);
-app.use("/api/cart",     cart);
-app.use("/api/admin", admin);
+app.use('/api/auth', auth);
+app.use('/api/products', products);
+app.use('/api/cart', cart);
+app.use('/api/admin', admin);
+app.use('/api/orders', orders);
 
 app.use((_req, _res, next) => {
-  next(new HttpError("Could not find this route", 404));
+  next(new HttpError('Could not find this route', 404));
 });
 
 app.use((error, _req, res, next) => {
   const status = error.statusCode || 500;
   const message = error.message;
   const data = error.data;
-  res.status(status).json({message, data});
+  res.status(status).json({ message, data });
 });
 
 await connectDB();
