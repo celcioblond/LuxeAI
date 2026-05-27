62771c5ee8e9e0d8260b6cebcd3e626cf9d087cc
mateo <mateoblondetz@gmail.com>
2026-05-26 22:54:29 -0500

    fix(server): increase product name and description max length

--- server/schemas/productSchema.js
+++ server/schemas/productSchema.js
@@ -1,9 +1,9 @@
-import z from "zod";
+import z from 'zod';
 
 export const productSchema = z.object({
-  name: z.string().min(5).max(50).trim(),
+  name: z.string().min(5).max(1000).trim(),
   price: z.number().positive(),
-  description: z.string().min(5).max(50),
+  description: z.string().min(5).max(1000),
   stock: z.number().min(0).int(),
   imageUrl: z.url(),
 });
